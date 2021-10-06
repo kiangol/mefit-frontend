ffbef6c3e4b39d526b77daf4f0c6025ed358aa7f
Are Gulliksen <aregulliksen@gmail.com>
2021-10-06 13:46:58 +0200

    Using actual url not variables in axios http for now.

--- src/api/http.js
+++ src/api/http.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 const http = Axios.create({
-    baseURL: `${process.env.BASE_URL}${process.env.API_VERSION}`,
-    //baseURL: "https://success-mefit.herokuapp.com/api",
+    //baseURL: `${process.env.BASE_URL}${process.env.API_VERSION}`,
+    baseURL: "https://success-mefit.herokuapp.com/api",
     withCredentials: true
 });
 
--- src/pages/AllExercises.jsx
+++ src/pages/AllExercises.jsx
@@ -19,10 +19,10 @@ const AllExercises = () => {
                 console.log("ERROR IN ALLEXERCISES: " + error);
                 setError(error);
             } else {
-                console.log(data)
+                console.log(data.data)
                 //console.log(JSON.stringify(data.data))
                 //console.log(JSON.parse(data.data))
-                //setExercises(data.data);
+                //setExercises(data);
             }
         };
         fetchData();
