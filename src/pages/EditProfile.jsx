5e01990aba7a82af7432e152e0fcfbf616be3c39
Karoline Wærstad <karoline_waer@hotmail.com>
2021-10-20 13:33:40 +0200

    FEAT: Edit profile button now works

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import withKeycloak from "../hoc/withKeycloak";
 import ProfileEdit from "../components/Profile/ProfileEdit";
+import styles from "../components/Profile/Profile.module.css";
+import KeycloakService from "../services/KeycloakService";
+import {listOne} from "../api/ProfileAPI";
+import WorkoutList from "../components/Workout/WorkoutList";
 
 const EditedProfile = () => {
 
+    const [profile, setProfile] = useState();
+    const [username] = useState({
+        username: KeycloakService.getUsername()
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const {data, error} = await listOne(username.username);
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(data);
+                setProfile(data);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <>
-            <ProfileEdit />
+            {profile && (
+                <ProfileEdit toEdit={profile}/>
+            )}
         </>
     )
 }
