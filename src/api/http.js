07958e260eb5f3949e9bf905b01f6bdd37ddd881
kiang <kiang@uio.no>
2021-10-22 12:10:26 +0200

    fix: conflict

--- src/api/http.js
+++ src/api/http.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 const http = Axios.create({
     //baseURL: `${process.env.BASE_URL}${process.env.API_VERSION}`,
     baseURL: "https://success-mefit.herokuapp.com/api",
-    //baseURL: "http://localhost:8080/api",
+    // baseURL: "http://localhost:8080/api",
     withCredentials: false
 });
 
